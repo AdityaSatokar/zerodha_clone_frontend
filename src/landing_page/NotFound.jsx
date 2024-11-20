72d6cc1c04b686a84aa2c755db7fb343d8840d7c
Aditya Satokar <145588107+AdityaSatokar@users.noreply.github.com>
2024-11-20 10:24:19 +0530

    Add files via upload

---
+++ src/landing_page/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div className='container p-5 mb-5'>
+        <div className="row text-center">
+            <h1 className='mt-5'>404 NOT FOUND</h1>
+            <p>Sorry the page you are looking for does not exists!</p>
+        </div>
+    </div>
+  )
+}
+
+export default Notfound
